ef92995de16cf34930f38fecd5dccf23e05f4574
Mas Azalya <masazalya@shimmersensing.com>
2025-05-09 19:42:17 +0800

    add 3r gyro sensor support

--- ShimmerExamplePlot/ShimmerExamplePlot/ContentView.swift
+++ ShimmerExamplePlot/ShimmerExamplePlot/ContentView.swift
@@ -263,6 +263,14 @@ struct ContentView: View {
                     }
                 }
                 })
+                Button("WriteInfoMem Gyro Shimmer3R",action:{ Task {
+                    do {
+                        await viewModel.sendInfoMemS3RGyro()
+                    } catch {
+                        print("Error: \(error)")
+                    }
+                }
+                })
             }
             Picker("Select EXG Gain", selection: $viewModel.exgGainIndex) {
                 ForEach(0..<viewModel.exgGain.count, id: \.self) { index in
@@ -331,6 +339,15 @@ struct ContentView: View {
                 .onChange(of: viewModel.lnAccelRangeIndex) { newValue in
                     // Update the ViewModel's wrRangeIndex property
                     viewModel.lnAccelRangeIndex = newValue
+                }
+                Picker("Select Gyro Range", selection: $viewModel.gyroRange3RIndex) {
+                    ForEach(0..<viewModel.gyroRange3R.count, id: \.self) { index in
+                        Text(viewModel.gyroRange3R[index])
+                    }
+                }
+                .onChange(of: viewModel.gyroRange3RIndex) { newValue in
+                    // Update the ViewModel's wrRangeIndex property
+                    viewModel.gyroRange3RIndex = newValue
                 }
                 Button("WriteInfoMem Shimmer3R",action:{ Task {
                     do {
